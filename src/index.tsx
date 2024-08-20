85c8745df0a24654bb1814baddbef617888e1084
shubham giri <41017859+ishubs@users.noreply.github.com>
2024-08-20 15:03:05 +0530

    fix build issue

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ import { rainbowKitConfig } from "lib/wallets/rainbowKitConfig";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { AarcEthWalletConnector } from "@aarc-dev/eth-connector"
 import "@aarc-dev/eth-connector/styles.css"
-import "@aarc-dev/deposit-widget/styles.css"
+// import "@aarc-dev/deposit-widget/styles.css"
 import {
   AarcSwitchWidgetProvider,
   FKConfig,
@@ -40,12 +40,12 @@ createRoot(document.getElementById("root")!).render(
 );
 
 
-function AarcProvider({ children }) {   
+function AarcProvider({ children }) {
 
   const { address, isConnected, connector, chainId } = useAccount();
 
   console.log(address, "address")
-    
+
   const config: FKConfig = {
     appName: "Dapp Name",
     module: {
